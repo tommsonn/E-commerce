6df30cf87bf1622070e7ee1b62c64956b58aab4d
thomas tesema <thomastes433@gmail.com>
2026-03-09 15:59:45 +0300

    Change API URL to production endpoint

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://tomshop-backend.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
